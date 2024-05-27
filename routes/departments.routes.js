e71c75aa1063e842aebd17745ee67bc9226ebf2f
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-27 12:21:37 +0200

    change the response in endpoints in the put and delete methods in departments.routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -56,7 +56,7 @@ router.put('/departments/:id', async (req, res) => {
     if(dep) {
       dep.name = name;
       await dep.save();
-      res.json({ message: 'OK' });
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -71,7 +71,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json(dep);
     }
     else res.status(404).json({ message: 'Not found...' });
   }
