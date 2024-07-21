46a283be20b7ed483aaef5b43e0b5044f1f223e6
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 16:04:24 +0200

    Add test if firstName is not a string

--- models/test/employee.test.js
+++ models/test/employee.test.js
@@ -5,11 +5,26 @@ const Employee = require('../employee.model.js');
 describe('Employee', () => {
 
   it('should throw an error if no "name" arg', async () => {
-    const dep = new Employee({});
+    const emp = new Employee({});
 
-    dep.validateSync(err => {
+    emp.validateSync(err => {
       expect(err.errors.name).to.exist;
     });  
+  });
+
+  it('should throw an error if firstName is not a string', () => {
+    const cases = [
+        {firstName: [], lastName: 'Doe', department: 'marketing'},
+        {firstName: {}, lastName: 'Doe', department: 'marketing'},
+    ];
+
+    for(let dataFirstName of cases) {
+      const emp = new Employee(dataFirstName);
+
+      emp.validateSync(err => {
+        expect(error).to.exist;
+      });  
+    }
   });
 
   after(() => {
