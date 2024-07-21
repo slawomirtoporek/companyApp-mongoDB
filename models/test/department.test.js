4eb27349390d5f9fc7d3f6c022554d70a56b8c5a
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 15:14:11 +0200

    Add test if name is okay

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -34,6 +34,17 @@ describe('Department', () => {
     }
   });
 
+  it('should not throw an error if "name" is okay', () => {
+    const cases = ['Management', 'Human Resources'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+  
+      dep.validateSync(err => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+
   after(() => {
     mongoose.models = {};
   });
