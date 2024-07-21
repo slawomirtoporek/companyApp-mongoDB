3e6e53d709e229cfbfafb53eb624dd12d902d089
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 15:48:00 +0200

    Add test if no name arg

--- models/test/employee.test.js
+++ models/test/employee.test.js
@@ -4,4 +4,15 @@ const Employee = require('../employee.model.js');
 
 describe('Employee', () => {
 
+  it('should throw an error if no "name" arg', async () => {
+    const dep = new Employee({});
+
+    dep.validateSync(err => {
+      expect(err.errors.name).to.exist;
+    });  
+  });
+
+  after(() => {
+    mongoose.models = {};
+  });
 });
