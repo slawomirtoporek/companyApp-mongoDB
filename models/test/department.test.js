c3254106599562171d01a105bb08b5baab75b252
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 14:48:24 +0200

    Add models/test/department.test.js

---
+++ models/test/department.test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const Department = require('../department.model.js');
+
+describe('Department', () => {
+
+  it('should throw an error if no "name" arg', () => {
+
+  });
+
+});
