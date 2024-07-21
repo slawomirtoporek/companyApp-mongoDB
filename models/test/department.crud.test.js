67de6504a727ad3c8dfcf931026a6c701ffff227
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 19:17:32 +0200

    Add test for the Updating data group in the department.crud.test.js

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -53,6 +53,43 @@ describe('Department', () => {
     });
   });
 
+  describe('Updating data', () => {
+
+    beforeEach(async () => {
+      const testDepOne = new Department({ name: 'Department #1' });
+      await testDepOne.save();
+    
+      const testDepTwo = new Department({ name: 'Department #2' });
+      await testDepTwo.save();
+    });
+
+    it('should properly update one document with "updateOne" method', async () => {
+      await Department.updateOne({ name: 'Department #1' }, { $set: { name: '=Department #1=' }});
+      const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+      expect(updatedDepartment).to.not.be.null;
+    });
+  
+    it('should properly update one document with "save" method', async () => {
+      const department = await Department.findOne({ name: 'Department #1' });
+      department.name = '=Department #1=';
+      await department.save();
+    
+      const updatedDepartment = await Department.findOne({ name: '=Department #1=' });
+      expect(updatedDepartment).to.not.be.null;
+    });
+  
+    it('should properly update multiple documents with "updateMany" method', async () => {
+      await Department.updateMany({}, { $set: { name: 'Updated!' }});
+      const departments = await Department.find({ name: 'Updated!' });
+      expect(departments.length).to.be.equal(2);
+    });
+
+    afterEach(async () => {
+      await Department.deleteMany();
+    });
+  
+  });
+
   after(() => {
     mongoose.models = {};
   });
