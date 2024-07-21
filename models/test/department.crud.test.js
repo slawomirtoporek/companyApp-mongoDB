14e7b4f62f66f91ff85ae2935840c91b87596a9a
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 16:26:52 +0200

    Install onchange and add hook before

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -4,6 +4,15 @@ const Department = require('../department.model');
 
 describe('Department', () => {
 
+  before(async () => {
+
+    try {
+      await mongoose.connect('mongodb://localhost:27017/companyDBtest', { useNewUrlParser: true, useUnifiedTopology: true });
+    } catch(err) {
+      console.error(err);
+    }
+  });
+
   after(() => {
     mongoose.models = {};
   });
