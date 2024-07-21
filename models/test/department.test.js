0401e5ca4914d259ebc1ecf0a2f27ebf475bfad8
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-21 14:52:35 +0200

    Add test if no name arg

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -1,10 +1,18 @@
 const expect = require('chai').expect;
+const mongoose = require('mongoose');
 const Department = require('../department.model.js');
 
 describe('Department', () => {
 
-  it('should throw an error if no "name" arg', () => {
+  it('should throw an error if no "name" arg', async () => {
+    const dep = new Department({}); // create new Department, but don't set `name` attr value
 
+    dep.validateSync(err => {
+      expect(err.errors.name).to.exist;
+    });  
   });
 
+  after(() => {
+    mongoose.models = {};
+  });
 });
